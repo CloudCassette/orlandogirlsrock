3b9af01a377418c27cd6f223b5843d210c75c989
cloudcassette <137094382+CloudCassette@users.noreply.github.com>
2026-02-09 22:30:58 -0500

    Added logo and buttons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   // Copy CSS files to output
   eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/ogrc-logo-white.svg");
 
   return {
     dir: {
